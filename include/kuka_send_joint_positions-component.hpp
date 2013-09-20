ac797dc0987c6815033880bedd24e235f7ba2a60
shak <sovannara.hak@isir.upmc.fr>
2013-09-20 14:48:44 +0200

    Filter command output

--- include/kuka_send_joint_positions-component.hpp
+++ include/kuka_send_joint_positions-component.hpp
@@ -5,6 +5,7 @@
 #include <kuka_lwr_fri/friComm.h>
 #include <motion_control_msgs/typekit/Types.hpp>
 #include <sensor_msgs/typekit/Types.hpp>
+#include <movingMean.hpp>
 
 using namespace RTT;
 
@@ -16,10 +17,14 @@ class Kuka_send_joint_positions : public RTT::TaskContext{
     void updateHook();
     void stopHook();
     void cleanupHook();
+
+    void setNumObs(unsigned int numObs);
+
     motion_control_msgs::JointPositions m_joint_pos_command;
     motion_control_msgs::JointVelocities m_joint_vel_command;
     tFriKrlData fri_to_krl;
     tFriKrlData fri_frm_krl;
+    MovingMean mean;
     double distance;
     int direction;
     RTT::InputPort<double> port_distance;
--- src/kuka_send_joint_positions-component.cpp
+++ src/kuka_send_joint_positions-component.cpp
@@ -10,12 +10,14 @@ Kuka_send_joint_positions::Kuka_send_joint_positions(std::string const& name) : 
   this->addPort("jointVelocitiesCommand", port_vel_command);
   this->addPort("toKRL", port_fri_to_krl);
   this->addPort("fromKRL", port_fri_frm_krl);
+  this->addOperation("setNumObs", &Kuka_send_joint_positions::setNumObs, this, RTT::OwnThread);
   std::cout << "Kuka_send_joint_positions constructed !" <<std::endl;
 }
 
 bool Kuka_send_joint_positions::configureHook(){
   distance = 0.0;
   direction = 1;
+  setNumObs(20);
 
   //initialize the arrays that will be send to KRL
   for(int i=0; i<16; ++i){
@@ -53,15 +55,15 @@ void Kuka_send_joint_positions::updateHook(){
         q.push_back(*iter);
     }// = dataJState.position;//dataJState.velocity  dataJState.effort
 
-    if(q[2] < -1.0471975512 && direction == 1){
+    if(q[2] > 1.0471975512 && direction == 1){
         direction = -1;
     }
-    else if( q[2] > 1.0471975512 && direction == -1){
+    else if( q[2] < -1.0471975512 && direction == -1){
         direction = 1;
     }
 
-    double alpha = 0.10 - std::min( std::max(distance, 0.10), 0.60 );
-    m_joint_vel_command.velocities[2] = 0.1 * alpha * direction;
+    double alpha = std::min( std::max(distance, 0.10), 0.60 ) - 0.10;
+    m_joint_vel_command.velocities[2] = mean.getMean( 0.1 * alpha * direction );
 
     port_vel_command.write(m_joint_vel_command);
 
@@ -82,6 +84,10 @@ void Kuka_send_joint_positions::stopHook() {
 
 void Kuka_send_joint_positions::cleanupHook() {
   std::cout << "Kuka_send_joint_positions cleaning up !" <<std::endl;
+}
+
+void Kuka_send_joint_positions::setNumObs(unsigned int numObs){
+  mean.setNumObs(numObs);
 }
 
 /*
